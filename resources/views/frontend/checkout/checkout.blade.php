ef993075b120f936b79409f9abb214f7079910c6
mostafizur1082 <mdmostafizurrahman108239@gmail.com>
2023-09-24 13:16:39 +0600

    complate cash on delivary

--- resources/views/frontend/checkout/checkout.blade.php
+++ resources/views/frontend/checkout/checkout.blade.php
@@ -20,7 +20,8 @@
     <!-- Checkout Area -->
     <section class="checkout-area pt-100 pb-70">
         <div class="container">
-            <form>
+            <form method="post" role="form" action="{{ route('checkout.store') }}">
+                @csrf
                 <div class="row">
                     <div class="col-lg-8">
                         <div class="billing-details">
@@ -31,13 +32,15 @@
                                     <div class="form-group">
                                         <label>Country <span class="required">*</span></label>
                                         <div class="select-box">
-                                            <select class="form-control">
-                                                <option value="5">United Arab Emirates</option>
-                                                <option value="1">China</option>
-                                                <option value="2">United Kingdom</option>
-                                                <option value="0">Germany</option>
-                                                <option value="3">France</option>
-                                                <option value="4">Japan</option>
+                                            <select name="country" class="form-control">
+                                                <option value="Bangladesh">Bangladesh</option>
+                                                <option value="India">India</option>
+                                                <option value="United Arab">United Arab Emirates</option>
+                                                <option value="China">China</option>
+                                                <option value="United Kingdom">United Kingdom</option>
+                                                <option value="Germany">Germany</option>
+                                                <option value="France">France</option>
+                                                <option value="Japan">Japan</option>
                                             </select>
                                         </div>
                                     </div>
@@ -45,64 +48,49 @@
 
                                 <div class="col-lg-6 col-md-6">
                                     <div class="form-group">
-                                        <label>First Name <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
+                                        <label>  Name <span class="required">*</span></label>
+                                        <input type="text" name="name" class="form-control" value="{{ \Auth::user()->name }}">
                                     </div>
                                 </div>
 
                                 <div class="col-lg-6 col-md-6">
                                     <div class="form-group">
-                                        <label>Last Name <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
+                                        <label>Email <span class="required">*</span></label>
+                                        <input type="email" name="email" class="form-control" value="{{ \Auth::user()->email }}">
                                     </div>
                                 </div>
 
-                                <div class="col-lg-12 col-md-12">
+                                <div class="col-lg-6 col-md-12">
                                     <div class="form-group">
-                                        <label>Company Name</label>
-                                        <input type="text" class="form-control">
+                                        <label>Phone</label>
+                                        <input type="text" name="phone" class="form-control" value="{{ \Auth::user()->phone }}">
                                     </div>
                                 </div>
 
-                                <div class="col-lg-12 col-md-6">
+                                <div class="col-lg-6 col-md-6">
                                     <div class="form-group">
                                         <label>Address <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-12 col-md-6">
-                                    <div class="form-group">
-                                        <label>Town / City <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
+                                        <input type="text" name="address" class="form-control" value="{{ \Auth::user()->address }}">
                                     </div>
                                 </div>
 
                                 <div class="col-lg-6 col-md-6">
                                     <div class="form-group">
-                                        <label>State / County <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
+                                        <label>State <span class="required">*</span></label>
+                                        <input type="text" name="state" class="form-control">
+                                        @if ($errors->has('state'))
+                                            <div class="text-danger">{{ $errors->first('state') }}</div>
+                                        @endif
                                     </div>
                                 </div>
 
                                 <div class="col-lg-6 col-md-6">
                                     <div class="form-group">
-                                        <label>Postcode / Zip <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>Email Address <span class="required">*</span></label>
-                                        <input type="email" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>Phone <span class="required">*</span></label>
-                                        <input type="text" class="form-control">
+                                        <label>ZipCode <span class="required">*</span></label>
+                                        <input type="text" name="zip_code" class="form-control">
+                                        @if ($errors->has('zip_code'))
+                                            <div class="text-danger">{{ $errors->first('zip_code') }}</div>
+                                        @endif
                                     </div>
                                 </div>
 
@@ -126,14 +114,16 @@
                                     <hr>
 
                                     <div style="display: flex">
-                                        <img style="height:100px; width:120px;object-fit: cover" src="{{ (!empty($room->image))?
-                                         url('upload/roomimg/'.$room->image):url('upload/no_image.jpg') }}" alt="Images" alt="Images">
+                                        <img style="height:100px; width:120px;object-fit: cover"
+                                            src="{{ !empty($room->image) ? url('upload/roomimg/' . $room->image) : url('upload/no_image.jpg') }}"
+                                            alt="Images" alt="Images">
                                         <div style="padding-left: 10px;">
-                                            <a href=" " style="font-size: 20px; color: #595959;font-weight: bold">{{ @$room->type->name }}</a>
+                                            <a href=" "
+                                                style="font-size: 20px; color: #595959;font-weight: bold">{{ @$room->type->name }}</a>
                                             <p><b>{{ $room->price }} / Night</b></p>
                                         </div>
 
-                                </div>
+                                    </div>
 
 
 
@@ -141,30 +131,51 @@
 
                                     <table class="table" style="width: 100%">
                                         @php
-                                      $subtotal = $room->price * $nights * $book_data['number_of_rooms'];
-                                      $discount =($room->discount/100)*$subtotal;
+                                            $subtotal = $room->price * $nights * $book_data['number_of_rooms'];
+                                            $discount = ($room->discount / 100) * $subtotal;
                                         @endphp
 
-                                            <tr>
-                                                <td><p>Total Night <br> <b> ( {{ $book_data['check_in'] }} - {{ $book_data['check_out'] }})</b></p></td>
-                                                <td style="text-align: right"><p> {{ $nights }} Days</p></td>
-                                            </tr>
-                                            <tr>
-                                                <td><p>Total Room</p></td>
-                                                <td style="text-align: right"><p>{{ $book_data['number_of_rooms'] }}</p></td>
-                                            </tr>
-                                            <tr>
-                                                <td><p>Subtotal</p></td>
-                                                <td style="text-align: right"><p>${{ $subtotal }}</p></td>
-                                            </tr>
-                                            <tr>
-                                                <td><p>Discount</p></td>
-                                                <td style="text-align:right"> <p>${{ $discount }}</p></td>
-                                            </tr>
-                                            <tr>
-                                                <td><p>Total</p></td>
-                                                <td style="text-align:right"> <p>${{ $subtotal-$discount }}</p></td>
-                                            </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Total Night <br> <b> ( {{ $book_data['check_in'] }} -
+                                                        {{ $book_data['check_out'] }})</b></p>
+                                            </td>
+                                            <td style="text-align: right">
+                                                <p> {{ $nights }} Days</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Total Room</p>
+                                            </td>
+                                            <td style="text-align: right">
+                                                <p>{{ $book_data['number_of_rooms'] }}</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Subtotal</p>
+                                            </td>
+                                            <td style="text-align: right">
+                                                <p>${{ $subtotal }}</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Discount</p>
+                                            </td>
+                                            <td style="text-align:right">
+                                                <p>${{ $discount }}</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Total</p>
+                                            </td>
+                                            <td style="text-align:right">
+                                                <p>${{ $subtotal - $discount }}</p>
+                                            </td>
+                                        </tr>
                                     </table>
 
                                 </div>
@@ -178,25 +189,16 @@
                         <div class="payment-box">
                             <div class="payment-method">
                                 <p>
-                                    <input type="radio" id="direct-bank-transfer" name="radio-group" checked>
-                                    <label for="direct-bank-transfer">Direct Bank Transfer</label>
-                                    Make your payment directly into our bank account. Please use your Order
-                                    ID as the payment reference. Your order will not be shipped until the funds have cleared
-                                    in our account.
-                                </p>
-                                <p>
-                                    <input type="radio" id="paypal" name="radio-group">
-                                    <label for="paypal">PayPal</label>
-                                </p>
-                                <p>
-                                    <input type="radio" id="cash-on-delivery" name="radio-group">
-                                    <label for="cash-on-delivery">Cash On Delivery</label>
-                                </p>
+                                    <input type="radio" id="cash-on-delivery" name="payment_method" value="COD">
+                                                 <label for="cash-on-delivery">Cash On Delivery</label>
+                                             </p>
+                                             <p>
+                                                 <input type="radio" id="paypal" name="radio-group">
+                                                 <label for="paypal">Stripe</label>
+                                             </p>
                             </div>
 
-                            <a href="#" class="order-btn three">
-                                Place to Order
-                            </a>
+                            <button type="submit" class="order-btn">Place to Order</button>
                         </div>
                     </div>
                 </div>
